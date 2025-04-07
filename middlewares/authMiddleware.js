ded38409209f4063af0036ad82a20332590e3e99
Dipesh Tamang <128952758+Devs-DH@users.noreply.github.com>
2025-04-07 16:43:50 +0900

    Update authMiddleware.js

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,7 +1,6 @@
 module.exports = (req, res, next) => {
-    // Check if session exists (user is logged in)
     if (!req.session.username || !req.session.role) {
         return res.status(401).json({ message: 'Unauthorized: No session data available.' });
     }
-    next();  // Proceed to the next route or middleware
+    next(); 
 };
